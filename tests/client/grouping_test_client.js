504d437d6b74e04dec45d82e8847f725e89fc305
harryadel <harryadelb@gmail.com>
2025-08-07 15:35:18 +0300

    Add grouping_test_clientjs

--- tests/client/grouping_test_client.js
+++ tests/client/grouping_test_client.js
@@ -1,29 +1,31 @@
-// const myGroup = "group1";
+import { initializeTestCollections } from "../utils.js";
 
-// hookCollection = (collection) => Partitioner.partitionCollection(collection);
+const myGroup = "group1";
 
-// /*
-// These tests need to all async so they are in the right order
-// */
+const groupingCollections = initializeTestCollections();
 
-// Tinytest.addAsync("partitioner - collections - join group", (test, next) => {
-//   Meteor.call("joinGroup", myGroup, (err, res) => {
-//     test.isFalse(err);
-//     next();
-//   });
-// });
+/*
+These tests need to all async so they are in the right order
+*/
 
-// // Ensure that the group id has been recorded before subscribing
-// Tinytest.addAsync("partitioner - collections - received group id", (test, next) => {
-//   Tracker.autorun((c) => {
-//     const groupId = Partitioner.group();
-//     if (groupId) {
-//       c.stop();
-//       test.equal(groupId, myGroup);
-//       next();
-//     }
-//   });
-// });
+Tinytest.addAsync("partitioner - collections - join group", (test, next) => {
+  Meteor.call("joinGroup", myGroup, (err, res) => {
+    test.isFalse(err);
+    next();
+  });
+});
+
+// Ensure that the group id has been recorded before subscribing
+Tinytest.addAsync("partitioner - collections - received group id", (test, next) => {
+  Tracker.autorun((c) => {
+    const groupId = Partitioner.group();
+    if (groupId) {
+      c.stop();
+      test.equal(groupId, myGroup);
+      next();
+    }
+  });
+});
 
 // Tinytest.addAsync("partitioner - collections - test subscriptions ready", (test, next) => {
 //   const handle = Meteor.subscribe("groupingTests");
@@ -38,7 +40,6 @@
 // Tinytest.addAsync("partitioner - collections - local empty find", async (test, next) => {
 //   test.equal(await basicInsertCollection.find().countAsync(), 0);
 //   test.equal(await basicInsertCollection.find({}).countAsync(), 0);
-//   next();
 // });
 
 // Tinytest.addAsync("partitioner - collections - remote empty find", (test, next) => {
@@ -63,30 +64,28 @@
 // ]);
 
 // testAsyncMulti("partitioner - collections - find from two groups", [
-//   async (test, expect) => {
-//     test.equal(await twoGroupCollection.find().countAsync(), 1);
+//   async (test) => {
+//     test.equal(await groupingCollections.twoGroup.find().countAsync(), 1);
 
-//     (await twoGroupCollection.find().fetchAsync()).forEach((el) => {
+//     (await groupingCollections.twoGroup.find().fetchAsync()).forEach((el) => {
 //       test.isFalse(el._groupId != null);
 //     });
 
-//     Meteor.call("getCollection", "twoGroup", expect((err, res) => {
-//       test.isFalse(err);
-//       test.equal(res.length, 2);
-//     }));
+//     const res = await Meteor.callAsync("getCollection", "twoGroup")
+//     test.isFalse(res.error);
+//     test.equal(res.length, 2);
 //   }
 // ]);
 
 // testAsyncMulti("partitioner - collections - insert into two groups", [
-//   async (test, expect) => {
-//     twoGroupCollection.insert({a: 2}, expect(async (err) => {
-//       test.isFalse(err, JSON.stringify(err));
-//       test.equal(await twoGroupCollection.find().countAsync(), 2);
+//   async (test) => {
+//     const res = await groupingCollections.twoGroup.insertAsync({a: 2})
+//     test.isFalse(res.error);
+//       test.equal(await groupingCollections.twoGroup.find().countAsync(), 2);
 
-//       (await twoGroupCollection.find().fetchAsync()).forEach((el) => {
+//       (await groupingCollections.twoGroup.find().fetchAsync()).forEach((el) => {
 //         test.isFalse(el._groupId != null);
 //       });
-//     }));
 //     /*
 //       twoGroup now contains
 //       { _groupId: "myGroup", a: 1 }
@@ -94,30 +93,27 @@
 //       { _groupId: "otherGroup", a: 1 }
 //     */
 //   },
-//   (test, expect) => {
-//     Meteor.call("getMyCollection", "twoGroup", expect((err, res) => {
-//       test.isFalse(err);
-//       test.equal(res.length, 2);
+//   async (test) => {
+//     const res = await Meteor.callAsync("getMyCollection", "twoGroup")
+//     test.isFalse(res.error);
+//     test.equal(res.length, 2);
 
 //       // Method finds should also not return _groupId
 //       res.forEach((el) => {
-//         test.isFalse(el._groupId != null);
-//       });
-//     }));
+//       test.isFalse(el._groupId != null);
+//     });
 //   },
-//   (test, expect) => { // Ensure that the other half is still on the server
-//     Meteor.call("getCollection", "twoGroup", expect((err, res) => {
-//       test.isFalse(err, JSON.stringify(err));
-//       test.equal(res.length, 3);
-//     }));
+//   async (test) => { // Ensure that the other half is still on the server
+//     const res = await Meteor.callAsync("getCollection", "twoGroup")
+//     test.isFalse(res.error);
+//     test.equal(res.length, 3);
 //   }
 // ]);
 
 // testAsyncMulti("partitioner - collections - server insert for client", [
-//   (test, expect) => {
-//     Meteor.call("serverInsert", "twoGroup", {a: 3}, expect((err, res) => {
-//       test.isFalse(err);
-//     }));
+//   async (test) => {
+//     const res = await Meteor.callAsync("serverInsert", "twoGroup", {a: 3})
+//     test.isFalse(res.error);
 //     /*
 //       twoGroup now contains
 //       { _groupId: "myGroup", a: 1 }
@@ -126,25 +122,23 @@
 //       { _groupId: "otherGroup", a: 1 }
 //     */
 //   },
-//   (test, expect) => {
-//     Meteor.call("getMyCollection", "twoGroup", {}, expect((err, res) => {
-//       test.isFalse(err);
-//       test.equal(res.length, 3);
+//   async (test) => {
+//     const res = await Meteor.callAsync("getMyCollection", "twoGroup", {})
+//     test.isFalse(res.error);
+//     test.equal(res.length, 3);
 
 //       res.forEach((el) => {
 //         test.isFalse(el._groupId != null);
 //       });
-//     }));
 //   }
 // ]);
 
 // testAsyncMulti("partitioner - collections - server update identical keys across groups", [
-//   (test, expect) => {
-//     Meteor.call("serverUpdate", "twoGroup",
+//   async (test) => {
+//     const res = await Meteor.callAsync("serverUpdate", "twoGroup",
 //       {a: 1},
-//       {$set: {b: 1}}, expect((err, res) => {
-//         test.isFalse(err);
-//       }));
+//       {$set: {b: 1}})
+//     test.isFalse(res.error);
 //     /*
 //       twoGroup now contains
 //       { _groupId: "myGroup", a: 1, b: 1 }
@@ -153,32 +147,27 @@
 //       { _groupId: "otherGroup", a: 1 }
 //     */
 //   },
-//   (test, expect) => { // Make sure that the other group's record didn't get updated
-//     Meteor.call("getCollection", "twoGroup", expect((err, res) => {
-//       test.isFalse(err);
+//   async (test) => { // Make sure that the other group's record didn't get updated
+//     const res = await Meteor.callAsync("getCollection", "twoGroup")
+//     test.isFalse(res.error);
 //       res.forEach((doc) => {
 //         if (doc.a === 1 && doc._groupId === myGroup) {
 //           test.equal(doc.b, 1);
 //         } else {
 //           test.isFalse(doc.b);
-//         }
-//       });
-//     }));
+//       }
+//     });
 //   }
 // ]);
 
 // testAsyncMulti("partitioner - collections - server remove identical keys across groups", [
-//   (test, expect) => {
-//     Meteor.call("serverRemove", "twoGroup",
-//       {a: 1}, expect((err, res) => {
-//         test.isFalse(err);
-//       }));
+//   async (test) => {
+//     const res = await Meteor.callAsync("serverRemove", "twoGroup", {a: 1})
+//     test.isFalse(res.error);
 //   },
-//   (test, expect) => { // Make sure that the other group's record didn't get updated
-//     Meteor.call("getCollection", "twoGroup", {a: 1}, expect((err, res) => {
-//       test.isFalse(err);
-//       test.equal(res.length, 1);
-//       test.equal(res[0].a, 1);
-//     }));
+//   async (test) => { // Make sure that the other group's record didn't get updated
+//     const res = await Meteor.callAsync("getCollection", "twoGroup", {a: 1});
+//     test.equal(res.length, 1);
+//     test.equal(res[0].a, 1);
 //   }
 // ]);
