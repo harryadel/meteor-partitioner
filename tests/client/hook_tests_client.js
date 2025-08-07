538dd69d3ebd03add5f3d8a0004ddfa5b96b6b9e
harryadel <harryadelb@gmail.com>
2025-08-07 15:35:04 +0300

    Add hook_tests_client

--- tests/client/hook_tests_client.js
+++ tests/client/hook_tests_client.js
@@ -1,85 +1,114 @@
-// import { TestFuncs } from "meteor/mizzao:partitioner";
-// import { createTestUser } from "../utils.js";
+import { TestFuncs } from "meteor/mizzao:partitioner";
+import { createTestUser } from "../utils.js";
 
-// const testGroupId = "test_group_client";
+const testGroupId = "test_group_client";
 
 
-// // XXX All async here to ensure ordering
-// Tinytest.addAsync("partitioner - hooks - add client group", async (test) => {
-//   const userId = await createTestUser();
-//   const originalUserId = Meteor.userId;
-//   Meteor.userId = () => userId;
+// XXX All async here to ensure ordering
+Tinytest.addAsync("partitioner - hooks - add client group", async (test) => {
+  const userId = await createTestUser();
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => userId;
   
-//   try {
-//     await Meteor.callAsync("joinGroup", testGroupId);
-//   } catch (e) {
-//     test.fail("This should not throw an error"); 
-//   } finally {
-//     Meteor.userId = originalUserId;
-//   }
+  try {
+    await Meteor.callAsync("joinGroup", testGroupId);
+  } catch (e) {
+    test.fail("This should not throw an error"); 
+  } finally {
+    Meteor.userId = originalUserId;
+  }
 
-// });
+});
 
-// Tinytest.addAsync("partitioner - hooks - vanilla client find", async (test) => {
+Tinytest.addAsync("partitioner - hooks - vanilla client find", async (test) => {
+  const ctx = {
+    args: []
+  };
+
+  const userId = await createTestUser();
+
+  TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
+  // Should have nothing changed
+  test.length(ctx.args, 0);
+
+  TestFuncs.userFindHook.call(ctx, userId, ctx.args[0], ctx.args[1]);
+  // Also nothing changed
+  test.length(ctx.args, 0);
+});
+
+// TODO: Fix this test
+// Tinytest.addAsync("partitioner - hooks - admin hidden in client find", async (test) => {
 //   const ctx = {
 //     args: []
 //   };
 
 //   const userId = await createTestUser();
+//   const originalUserId = Meteor.userId;
+//   Meteor.userId = () => userId;
 
 //   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
 //   // Should have nothing changed
 //   test.length(ctx.args, 0);
 
-//   TestFuncs.userFindHook.call(ctx, userId, ctx.args[0], ctx.args[1]);
-//   // Also nothing changed
-//   test.length(ctx.args, 0);
+//   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
+//   // Admin removed from find
+//   test.equal(ctx.args[0].admin.$exists, false);
+
+//   Meteor.userId = originalUserId;
 // });
 
-// Tinytest.add("partitioner - hooks - set admin", (test) => {
-//   Meteor.call("setAdmin", true, (err, res) => {
-//     test.isFalse(err);
-//     test.isTrue(Meteor.users.findOne(Meteor.userId()).admin);
-//   });
+// TODO: Fix this test
+// Tinytest.addAsync("partitioner - hooks - admin hidden in selector find", async (test) => {
+//   const ctx = {
+//     args: [{foo: "bar"}]
+//   };
+
+//   const userId = await createTestUser();
+//   const originalUserId = Meteor.userId;
+//   Meteor.userId = () => userId;
+
+//   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
+//   // Should have nothing changed
+//   test.length(ctx.args, 1);
+//   test.equal(ctx.args[0].foo, "bar");
+
+//   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
+//   // Admin removed from find
+//   test.equal(ctx.args[0].foo, "bar");
+//   test.equal(ctx.args[0].admin.$exists, false);
+
+//   Meteor.userId = originalUserId;
 // });
 
-// // Tinytest.addAsync("partitioner - hooks - admin hidden in client find", (test, next) => {
-// //   const ctx = {
-// //     args: []
-// //   };
+// Need to remove admin to avoid fubars in other tests
+Tinytest.addAsync("partitioner - hooks - unset admin", async (test) => {
+  const userId = await createTestUser();
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => userId;
 
-// //   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
-// //   // Should have nothing changed
-// //   test.length(ctx.args, 0);
+  try {
+    await Meteor.callAsync("setAdmin", false);
+  } catch (e) {
+    test.fail("This should not throw an error");
+  } finally {
+    Meteor.userId = originalUserId;
+  }
 
-// //   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
-// //   // Admin removed from find
-// //   test.equal(ctx.args[0].admin.$exists, false);
-// //   next();
-// // });
+  test.isFalse(Meteor.user().admin);
+});
 
-// // Tinytest.addAsync("partitioner - hooks - admin hidden in selector find", (test, next) => {
-// //   const ctx = {
-// //     args: [{foo: "bar"}]
-// //   };
+Tinytest.addAsync("partitioner - hooks - set admin", async (test) => {
+  const userId = await createTestUser();
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => userId;
 
-// //   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
-// //   // Should have nothing changed
-// //   test.length(ctx.args, 1);
-// //   test.equal(ctx.args[0].foo, "bar");
+  try {
+    await Meteor.callAsync("setAdmin", true);
+  } catch (e) {
+    test.fail("This should not throw an error");
+  } finally {
+    Meteor.userId = originalUserId;
+  }
 
-// //   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
-// //   // Admin removed from find
-// //   test.equal(ctx.args[0].foo, "bar");
-// //   test.equal(ctx.args[0].admin.$exists, false);
-// //   next();
-// // });
-
-// // Need to remove admin to avoid fubars in other tests
-// // Tinytest.addAsync("partitioner - hooks - unset admin", async (test, next) => {
-// //   Meteor.call("setAdmin", false, (err, res) => {
-// //     test.isFalse(err);
-// //     test.isFalse(Meteor.user().admin);
-// //     next();
-// //   });
-// // });
+  test.isTrue(Meteor.user().admin);
+});
