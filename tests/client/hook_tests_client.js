fab1444107f1e6962c56a57d304db6431c84bdc8
harryadel <harryadelb@gmail.com>
2025-08-22 09:09:56 +0300

    Add tests for admin visibility in user find hooks

--- tests/client/hook_tests_client.js
+++ tests/client/hook_tests_client.js
@@ -36,49 +36,83 @@ Tinytest.addAsync("partitioner - hooks - vanilla client find", async (test) => {
   test.length(ctx.args, 0);
 });
 
-// TODO: Fix this test
-// Tinytest.addAsync("partitioner - hooks - admin hidden in client find", async (test) => {
-//   const ctx = {
-//     args: []
-//   };
+Tinytest.addAsync("partitioner - hooks - admin added in client find", async (test) => {
+  const ctx = {
+    args: []
+  };
 
-//   const userId = await createTestUser();
-//   const originalUserId = Meteor.userId;
-//   Meteor.userId = () => userId;
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => "fakeUserId";
 
-//   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
-//   // Should have nothing changed
-//   test.length(ctx.args, 0);
+  const originalUser = Meteor.user;
+  Meteor.user = () => ({admin: true});
 
-//   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
-//   // Admin removed from find
-//   test.equal(ctx.args[0].admin.$exists, false);
+  TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
+  // Should have nothing changed
+  test.length(ctx.args, 0);
 
-//   Meteor.userId = originalUserId;
-// });
+  TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
+  // Admin removed from find
+  test.equal(ctx.args[0].admin.$exists, false);
 
-// TODO: Fix this test
-// Tinytest.addAsync("partitioner - hooks - admin hidden in selector find", async (test) => {
-//   const ctx = {
-//     args: [{foo: "bar"}]
-//   };
+  Meteor.user = originalUser;
+  Meteor.userId = originalUserId;
+});
 
-//   const userId = await createTestUser();
-//   const originalUserId = Meteor.userId;
-//   Meteor.userId = () => userId;
+Tinytest.addAsync("partitioner - hooks - admin hidden in client find", async (test) => {
+  const ctx = {
+    args: []
+  };
 
-//   TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
-//   // Should have nothing changed
-//   test.length(ctx.args, 1);
-//   test.equal(ctx.args[0].foo, "bar");
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => "fakeUserId";
 
-//   TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
-//   // Admin removed from find
-//   test.equal(ctx.args[0].foo, "bar");
-//   test.equal(ctx.args[0].admin.$exists, false);
+  const originalUser = Meteor.user;
+  Meteor.user = () => ({admin: true});
 
-//   Meteor.userId = originalUserId;
-// });
+  try {
+    TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
+    // Should have nothing changed
+    test.length(ctx.args, 0);
+
+    TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
+    // Admin removed from find
+    test.equal(ctx.args[0].admin.$exists, false);
+  } finally {
+    Meteor.user = originalUser;
+    Meteor.userId = originalUserId;
+  }
+});
+
+Tinytest.addAsync("partitioner - hooks - admin hidden in selector find", async (test) => {
+  const ctx = {
+    args: [{foo: "bar"}]
+  };
+
+  const originalUserId = Meteor.userId;
+  Meteor.userId = () => "fakeUserId";
+
+  TestFuncs.userFindHook.call(ctx, undefined, ctx.args[0], ctx.args[1]);
+  // Should have nothing changed
+  test.length(ctx.args, 1);
+  test.equal(ctx.args[0].foo, "bar");
+
+  const originalUser = Meteor.user;
+  const originalIsDirect = Helpers.isDirectUserSelector;
+  Meteor.user = () => ({admin: true});
+  Helpers.isDirectUserSelector = () => false;
+
+  try {
+    TestFuncs.userFindHook.call(ctx, Meteor.userId(), ctx.args[0], ctx.args[1]);
+    // Admin removed from find
+    test.equal(ctx.args[0].foo, "bar");
+    test.equal(ctx.args[0].admin.$exists, false);
+  } finally {
+    Meteor.user = originalUser;
+    Helpers.isDirectUserSelector = originalIsDirect;
+    Meteor.userId = originalUserId;
+  }
+});
 
 // Need to remove admin to avoid fubars in other tests
 Tinytest.addAsync("partitioner - hooks - unset admin", async (test) => {
